d5f759f1ff2659c8878a9b4715ff24bc0c93db54
AuthGuards <commkgc@gmail.com>
2026-04-11 18:30:10 -0400

    Update Discord link to new URL

--- src/setup/constants.ts
+++ src/setup/constants.ts
@@ -1,5 +1,5 @@
 export const APP_VERSION = import.meta.env.PACKAGE_VERSION;
-export const DISCORD_LINK = "https://discord.gg/5eWu9Vz6tQ";
+export const DISCORD_LINK = "https://discord.authguards.com/";
 export const GITHUB_LINK = "https://github.com/steveyout/streamwatch";
 export const TWITTER_LINK = "";
 export const GA_ID = import.meta.env.VITE_GA_ID;
